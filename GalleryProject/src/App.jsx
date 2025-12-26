9bcadcc5c4f006567f9e6451caa1079215685a5e
BHAVIKKUKADIYA <kukadiyabhavik07@gmail.com>
2025-12-26 18:49:54 +0530

    fetch api with the axios

--- GalleryProject/src/App.jsx
+++ GalleryProject/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =()=>{
+  
+const getData=async ()=>{
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const response =await axios.get('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20');
+    console.log(response.data)
 }
 
-export default App
+    return(
+        <div>
+            <button onClick={getData}
+            className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>Click Me</button>
+        </div>
+    )
+}
+export default App;
